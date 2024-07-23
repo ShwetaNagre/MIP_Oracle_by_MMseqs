4ba856b33ad8ccd3648e2f123b74bd435eb1b700
ShwetaNagre <110716128+ShwetaNagre@users.noreply.github.com>
2024-07-23 09:00:28 +0200

    Update MIP_ORACLE.sh

--- MIP_ORACLE.sh
+++ MIP_ORACLE.sh
@@ -61,13 +61,99 @@ echo
 now=$(date +"%T")
 echo "Current time : $now"
 echo "__________________________________________"
-echo "MMSEQS2 input files have been generated. Running MMSEQS2 now."
-mmseqs easy-search whole_region.txt $parameterJ/mmseqs mmseqs2_result tmp --search-type 2
+echo
+### Download the NCBI Taxonomy Database
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
+mkdir taxonomy && tar -xxvf taxdump.tar.gz -C taxonomy
 echo
 now=$(date +"%T")
 echo "Current time : $now"
 echo "__________________________________________"
+echo "Downloaded the NCBI Taxonomy Database."
+echo
+### Extract the FASTA and Taxonomy Mapping Files
+blastdbcmd -db $parameterJ/nt -entry all > nt.fna
+blastdbcmd -db $parameterJ/nt -entry all -outfmt "%a %T" > nt.fna.taxidmapping
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Extracted the FASTA and Taxonomy Mapping Files."
+echo
+### Create MMSeqs2 Database and taxonomic database
+mmseqs createdb nt.fna nt.fnaDB
+mmseqs createtaxdb nt.fnaDB tmp --ncbi-tax-dump taxonomy/ --tax-mapping-file nt.fna.taxidmapping
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Created MMSeqs2 Database and taxonomic database."
+echo
+### filter database to only include human sequences
+mmseqs filtertaxseqdb nt.fnaDB seqTaxHumanDB --taxon-list 9606
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Filtered database to only include human sequences."
+echo
+### filter database to exclude human sequences
+mmseqs filtertaxseqdb nt.fnaDB seqTaxNonHumanDB --taxon-list '!9606'
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Filtered database to exclude human sequences."
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+### Trying taxonomy search with whole_region.txt file
+echo
+# Creating a database for whole_region.txt
+mmseqs createdb whole_region.txt whole_region_DB
+
+mmseqs createtaxdb whole_region_DB tmp --ncbi-tax-dump taxonomy/ --tax-mapping-file /nfs_master/shweta/New_MIPS/shweta_mips/tax_mmseq/nt.fna.taxidmapping
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Created a database for whole_region.txt"
+echo "__________________________________________"
+# Search against the database of human sequences
+mmseqs search whole_region_DB seqTaxHumanDB Human_Hits_for_QUERY tmp --search-type 2
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Searched against the database of human sequences."
+echo "__________________________________________"
+### Creating final tabular output
+mmseqs convertalis whole_region_DB seqTaxHumanDB Human_Hits_for_QUERY Human_Hits_for_QUERY.tab 
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Created final tabular output for human hits."
+echo "__________________________________________"
+# Search against the database of Non-human sequences
+mmseqs search whole_region_DB seqTaxNonHumanDB NonHuman_Hits_for_QUERY tmp --search-type 2 --max-seqs 10 --db-load-mode 2
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Searched against the database of Non-human sequences."
+echo "__________________________________________"
+### Creating final tabular output
+mmseqs convertalis whole_region_DB seqTaxNonHumanDB NonHuman_Hits_for_QUERY NonHuman_Hits_for_QUERY.tab 
+echo
+now=$(date +"%T")
+echo "Current time : $now"
+echo "__________________________________________"
+echo "Created final tabular output for Non-human hits."
+echo "__________________________________________"
 echo "MMSEQS2 results have been generated."
+echo "__________________________________________"
 python MIP_4.py
 echo
 now=$(date +"%T")
