44350b205ce9bb882bfd5693f45f7c816415527d
ShwetaNagre <110716128+ShwetaNagre@users.noreply.github.com>
2024-07-23 08:56:25 +0200

    Update MIP_4.py

--- MIP_4.py
+++ MIP_4.py
@@ -6,13 +6,21 @@
 import pandas as pd
 
 # Define file paths
-mmseqs_result_file = 'mmseqs2_result'
+mmseqs_human_hits_result_file = 'Human_Hits_for_QUERY.tab'
+mmseqs_nonhuman_hits_result_file = 'NonHuman_Hits_for_QUERY.tab'
 whole_region_file = 'whole_region.txt'
-no_hits_file = 'no_hits_ids.txt'
+no_human_hits_file = 'no_human_hits_ids.txt'
+no_nonhuman_hits_file = 'no_nonhuman_hits_ids.txt'
+final_no_hits_file = 'final_no_hits_ids.txt'
 
 # Read the MMseqs result file to get the IDs with hits
-hits_ids = pd.read_csv(mmseqs_result_file, sep='\t', header=None)
-hits_ids_set = set(hits_ids[0].tolist())
+## First for humans
+human_hits_ids = pd.read_csv(mmseqs_human_hits_result_file, sep='\t', header=None)
+human_hits_ids_set = set(human_hits_ids[0].tolist())
+
+## Now, for non-humans
+nonhuman_hits_ids = pd.read_csv(mmseqs_nonhuman_hits_result_file, sep='\t', header=None)
+nonhuman_hits_ids_set = set(nonhuman_hits_ids[0].tolist())
 
 # Read the whole_region.txt file to get all query IDs
 query_ids = []
@@ -22,12 +30,30 @@ with open(whole_region_file, 'r') as f:
             query_id = line.strip()[1:]  # Remove '>' for matching purposes
             query_ids.append(query_id)
 
-# Identify the IDs that did not get any hits
-no_hits_ids = [query_id for query_id in query_ids if query_id not in hits_ids_set]
+# Identify the IDs that did not get any hits in both human and non-human databases
+no_human_hits_ids = [query_id for query_id in query_ids if query_id not in human_hits_ids_set]
 
-# Write the IDs with no hits to a file
-with open(no_hits_file, 'w') as f:
-    for no_hit_id in no_hits_ids:
-        f.write('>' + no_hit_id + '\n')  # Add '>' back when writing to file
+no_nonhuman_hits_ids = [query_id for query_id in query_ids if query_id not in nonhuman_hits_ids_set]
 
-print(f"Total sequences with no hits: {len(no_hits_ids)}")
+# Write the IDs with no human hits to a file
+with open(no_human_hits_file, 'w') as f:
+    for no_human_hit_id in no_human_hits_ids:
+        f.write('>' + no_human_hit_id + '\n')  # Add '>' back when writing to file
+
+print(f"Total sequences with no human hits: {len(no_human_hits_ids)}")
+
+# Write the IDs with no non-human hits to a file
+with open(no_nonhuman_hits_file, 'w') as f:
+    for no_nonhuman_hits_id in no_nonhuman_hits_ids:
+        f.write('>' + no_nonhuman_hits_id + '\n')  # Add '>' back when writing to file
+
+print(f"Total sequences with no non-human hits: {len(no_nonhuman_hits_ids)}")
+
+# Concatenate the two files into a single file
+with open(final_no_hits_file, 'w') as outfile:
+    with open(no_human_hits_file, 'r') as infile:
+        outfile.write(infile.read())
+    with open(no_nonhuman_hits_file, 'r') as infile:
+        outfile.write(infile.read())
+
+print(f"Concatenated file created: {final_no_hits_file}")
